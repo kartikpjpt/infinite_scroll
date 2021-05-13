28aae78bd4a2a22ff1afe2ecf0a3fc14c418af43
kartik-280 <kr.pjpt@gmail.com>
2021-05-13 21:41:08 +0530

    Updated Website for Slow Internet

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = "obetMWoIOSTplyP2a6Ax_ElTwLJEO-2JNRLtSHzn188";
-const apiUrl =  `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+let apiUrl =  `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 let ready = false;
 let imagesLoaded = 0;
 let totalImages=0;
 
-photosArray =[];
-//Helper function to set attributes
-// function setAttributes(element, attributes)
-// {   for(const key in attributes) {
-//         element.setAttribute(attributes[key]);
-// }
-// }
-
+let photosArray =[];
 
 function imageLoaded() {
     console.log('Image loaded');
     imagesLoaded++;
     if(imagesLoaded==totalImages)
-    {   loader.hidden =true;
-        ready = true;
-        console.log("ready: ",true);
+    {   ready = true;
+        loader.hidden =true;
+        count = 30;
+        apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+        console.log(apiUrl);
     }
 }
-
 
 function displayPhotos() {
     imagesLoaded = 0;
@@ -38,27 +32,18 @@ function displayPhotos() {
         const item = document.createElement('a');
         item.setAttribute('href',photo.links.html);
         item.setAttribute('target','_blank');
-        // setAttributes(item, {
-        //     href:photo.links.html,
-        //     target:'_blank',
-        // });
         //Create <img> element
         const img = document.createElement('img');
         img.setAttribute('src',photo.urls.regular);
         img.setAttribute('alt',photo.alt_description);
         img.setAttribute('title',photo.alt_description);
-        // setAttributes(img,{
-        //     src:photo.urls.regular,
-        //     alt:photo.alt_description,
-        //     title:photo.alt_description,
-        // });
+        //Loading images 
         img.addEventListener('load',imageLoaded);
         //put a inside img
         item.appendChild(img);
         imageContainer.appendChild(item); 
     });
 }
-
 //Getting our data using async and await
 async function getPhotos() {
     try{
@@ -70,13 +55,11 @@ async function getPhotos() {
         // blank
     }
 }
-
 window.addEventListener('scroll', () => {
     if(window.innerHeight+window.scrollY >= document.body.offsetHeight - 1000 && ready) {
         ready=false;
         getPhotos();
     }
 })
-
 //calling the function
 getPhotos();
